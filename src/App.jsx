08606167d1479093b5221934bb3755e069f9327b
AtharvaPanegai <panegai.atharv@gmail.com>
2024-08-25 16:04:49 +0530

    Sign In Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import './index.css'
+import SignInPage from './Pages/SignInPage'
 import SignUpPage from './Pages/SignUpPage'
 
 function App() {
   return (
     <>
-      <SignUpPage />
+      <SignInPage />
     </>
   )
 }
---
+++ src/Pages/SignInPage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignInPageComponent from '../../Components/SignInPageComponent'
+
+const SignInPage = () => {
+  return (
+    <>
+    <SignInPageComponent />
+    </>
+  )
+}
+
+export default SignInPage
