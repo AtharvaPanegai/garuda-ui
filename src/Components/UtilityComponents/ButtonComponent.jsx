3adb69501af5f3bac375325c858949b806c92868
AtharvaPanegai <panegai.atharv@gmail.com>
2024-08-26 17:14:15 +0530

    Adding navbar, hero section, footer and cta section

---
+++ src/Components/UtilityComponents/ButtonComponent.jsx
@@ -0,0 +1,27 @@
+/** @format */
+
+const ButtonComponent = ({ size, text, colorScheme, onClickFunction }) => {
+  const colorClasses = {
+    blue: "bg-blue-500 hover:bg-blue-700 text-white",
+    green: "bg-green-500 hover:bg-green-700 text-white",
+    slate: "bg-slate-500 hover:bg-indigo-500 text-white",
+  };
+
+  const sizeClasses = {
+    small: "px-3 py-2 text-xs", 
+    medium: "px-5 py-2.5 text-sm",
+    large: "px-6 py-3 text-base",
+  };
+
+  return (
+    <button
+      className={`block rounded-md ${
+        colorClasses[colorScheme] || colorClasses.slate
+      } ${sizeClasses[size] || sizeClasses.medium} font-medium transition`}
+      onClick={onClickFunction}>
+      {text}
+    </button>
+  );
+};
+
+export default ButtonComponent;
