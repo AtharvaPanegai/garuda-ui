ded305b2fd5c0be15c0d380449d6a8e20e31ed04
AtharvaPanegai <panegai.atharv@gmail.com>
2024-12-07 17:00:21 +0530

    Issue-18 || Adding new settings page in the ui

---
+++ src/Pages/SettingsPage/index.jsx
@@ -0,0 +1,12 @@
+import SettingsPageComponent from '../../Components/SettingsPageComponent'
+import React from 'react'
+
+const SettingsPage = () => {
+  return (
+    <>
+        <SettingsPageComponent />
+    </>
+    )
+}
+
+export default SettingsPage
